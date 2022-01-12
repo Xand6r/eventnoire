f407e21d8c8173a8e5687972f22d24b128f2570f
xander <shuaibualexander@gmail.com>
2022-01-12 21:28:30 +0100

    fix first section

--- components/eventnoire/Eventnoire.js
+++ components/eventnoire/Eventnoire.js
@@ -35,7 +35,7 @@ function Eventnoire() {
                   className={styles.dates_img}
                   alt=""
                 />
-                <p>Feb 18th-20th, 2022</p>
+                <p>Cleaveland, Ohio</p>
               </div>
             </div>
             <button className={styles.btn}>Get Tickets</button>
@@ -58,7 +58,7 @@ function Eventnoire() {
               </div>
               <div className={styles.dates}>
                 <Image src={Location} width={17.5} height={20.75} alt="" />
-                <p>Feb 18th-20th, 2022</p>
+                <p>Los Angeles, CA</p>
               </div>
             </div>
             <button className={styles.btn}>Get Tickets</button>
